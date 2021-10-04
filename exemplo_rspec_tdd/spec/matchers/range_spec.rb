4bc4f21ddbaed47e4ae2dad66391a9eab89edb22
Fernando Mendes <fernando.msantos1@gmail.com>
2021-10-04 22:58:36 +0000

    Exemplo de Composição

--- exemplo_rspec_tdd/spec/matchers/range_spec.rb
+++ exemplo_rspec_tdd/spec/matchers/range_spec.rb
@@ -1,8 +1,19 @@
 describe (1..5), 'Range' do
+
   it '#cover' do
+    # inferência
+    is_expected.to cover(2)
+    #
     expect(subject).to cover(2)
     expect(subject).to cover(2,5)
     expect(subject).not_to cover(0,6)
 
   end
+
+  #sintaxe de 1 linha
+  it { is_expected.to cover(2) }
+  it { is_expected.to cover(2,5) }
+  it { is_expected.not_to cover(0,6) }
+
+  it { expect(subject).not_to cover(0,6) }
 end
