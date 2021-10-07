5340f20834b009964fa9e41eab98fa9cd28bf7fe
Fernando Mendes <fernando.msantos1@gmail.com>
2021-10-07 22:27:44 +0000

    Ajusta helper e adiciona Hooks before/after

--- exemplo_rspec_tdd/spec/composicao/helper.rb
+++ exemplo_rspec_tdd/spec/composicao/helper.rb
@@ -1,7 +1,12 @@
 module HelperComposicao
-
+  #HelperComposicao::NUMBERS
+  NUMBERS = [1,2,3]
+  
   def fruta
     %w(banana laranja uva).sample
   end
   
+  #HelperComposicao.fruta
+  #HelperComposicao::fruta
+  module_function :fruta
 end
--- exemplo_rspec_tdd/spec/matchers/atributos_spec.rb
+++ exemplo_rspec_tdd/spec/matchers/atributos_spec.rb
@@ -1,15 +1,39 @@
 require "pessoa"
 
 describe 'Atributos' do
-  it 'have attributes' do
-    pessoa = Pessoa.new
-    pessoa.nome = "Ruby"
-    pessoa.idade = 21
-    expect(pessoa).to have_attributes(
+
+  # around
+  # around(:each) do |teste|
+  #   teste.run
+  # end
+
+  before(:each) do
+    p "ANTES >>>"
+    @pessoa = Pessoa.new
+    p "#{@pessoa.inspect}"
+  end
+  
+  after(:each) do
+    p "DEPOIS >>>"
+    p "#{@pessoa.inspect}"
+  end
+  
+  it 'have attributes 1' do
+    @pessoa.nome = "Ruby"
+    @pessoa.idade = 21
+    expect(@pessoa).to have_attributes(
       nome: start_with("R") ,
       idade: be >=20 
     )
   end
 
+  it 'have attributes 2' do
+    @pessoa.nome = "Rails"
+    @pessoa.idade = 21
+    expect(@pessoa).to have_attributes(
+      nome: start_with("R") ,
+      idade: be >=20 
+    )
+  end
 
 end
