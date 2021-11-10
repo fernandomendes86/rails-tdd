b00b2bb68f587676724e1aacedc51ef25cd3f5df
Fernando Mendes <fernando.msantos1@gmail.com>
2021-11-10 00:38:50 +0000

    Usando has_many

--- test_app/app/models/customer.rb
+++ test_app/app/models/customer.rb
@@ -1,5 +1,7 @@
 class Customer < ApplicationRecord
 
+  has_many :orders
+
   def full_name
     "Sr. #{name}"
   end
--- test_app/spec/models/order_spec.rb
+++ test_app/spec/models/order_spec.rb
@@ -2,15 +2,22 @@ require 'rails_helper'
 
 RSpec.describe Order, type: :model do
   
-  it 'Tem 1 pedido' do
+  it 'belongs_to' do
     order = create(:order)
     expect(order.customer).to be_kind_of(Customer)
   end
 
-  it 'Tem 3 pedidos' do
+  it 'Possui 3 pedidos - create_list' do
     orders = create_list(:order, 3)
     p orders.inspect
     expect(orders.count).to eq(3)
+  end
+
+  it 'has many' do
+    customer = create(:customer_with_orders)
+    p customer.inspect
+    p customer.orders.inspect
+    expect(customer.orders.count).to eq(3)
   end
 
 end
