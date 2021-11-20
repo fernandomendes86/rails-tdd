7543bfbe2885471dc7ffdd59b47fe5ba475a0855
Fernando Mendes <fernando.msantos1@gmail.com>
2021-11-20 13:24:27 +0000

    Usando TimeHelpers

--- test_app/spec/models/customer_spec.rb
+++ test_app/spec/models/customer_spec.rb
@@ -37,5 +37,15 @@ RSpec.describe Customer, type: :model do
     expect(customer.vip).to eq(true)
   end
 
+  it 'travel to' do
+    travel_to Time.zone.local(2004, 11, 23, 01, 04, 44) do
+      @customer = create(:customer_vip)
+    end
+
+    #expect(@customer.created_at).to eq(Time.new(2004, 11, 23, 01, 04, 44))
+    expect(@customer.created_at).to be < Time.now
+
+  end
+
   it { expect { create(:customer) }.to change { Customer.all.size }.by(1) }
 end
