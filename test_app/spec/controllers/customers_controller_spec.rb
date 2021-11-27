7f2174fb84187f8eb19e66fca4a66a6c9a44295c
Fernando Mendes <fernando.msantos1@gmail.com>
2021-11-27 12:56:33 +0000

    Testes com route, content_type, flash notice e attributes_for

--- test_app/spec/controllers/customers_controller_spec.rb
+++ test_app/spec/controllers/customers_controller_spec.rb
@@ -46,24 +46,56 @@ RSpec.describe CustomersController, type: :controller do
   
   describe 'as Logged Member' do
 
-    it 'responds a 20' do
-      member = create(:member)
-      customer = create(:customer)
-    
-      sign_in member
-    
-      get :show, params: { id: customer.id }
+    before do
+      @member = create(:member)
+      @customer = create(:customer)
+    end
+
+    it 'Route' do
+      is_expected.to route(:get, '/customers').to(action: :index)
+    end
+
+    it 'Content-Type JSON' do
+      customer_params = attributes_for(:customer)
+      sign_in @member
+      post :create, format: :json, params: { customer: customer_params }
+      expect(response.content_type).to eq('application/json')
+    end
+
+    it 'Flash notice' do
+      customer_params = attributes_for(:customer)
+      sign_in @member
+      post :create, params: { customer: customer_params }
+      expect(flash[:notice]).to match(/successfully created/)
+    end
+
+    it 'with invalid attributes' do
+      customer_params = attributes_for(:customer, address: nil)
+      sign_in @member
+      expect {
+        post :create, params: { customer: customer_params }
+      }.not_to change(Customer, :count)
+    end
+
+    it 'with valid attributes' do
+      customer_params = attributes_for(:customer)
+      sign_in @member
+      expect { 
+        post :create, params: { customer: customer_params }
+      }.to change { Customer.count }.by(1)
+    end
+
+    it 'responds a 200 response' do
+
+      sign_in @member
+      get :show, params: { id: @customer.id }
       expect(response).to have_http_status(200)
 
     end
 
     it 'render a template :show' do
-      member = create(:member)
-      customer = create(:customer)
-    
-      sign_in member
-    
-      get :show, params: { id: customer.id }
+      sign_in @member
+      get :show, params: { id: @customer.id }
       expect(response).to render_template(:show)
 
     end
