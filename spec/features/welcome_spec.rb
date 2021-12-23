023c9dee7b787e72ba7c3626570854dc50baf662
Fernando Mendes <fernando.msantos1@gmail.com>
2021-12-23 00:43:55 +0000

    App com TDD

---
+++ spec/features/welcome_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+feature "Welcomes", type: :feature do
+
+  scenario 'Mostra a mensagem de Bem-Vindo' do
+    visit('/')
+    expect(page).to have_content('Bem-Vindo')  
+  end
+end
