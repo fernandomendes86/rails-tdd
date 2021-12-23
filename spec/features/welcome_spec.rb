2c3ddae2180becc0f1ad7e4ff24714aaca77ff23
Fernando Mendes <fernando.msantos1@gmail.com>
2021-12-23 16:34:22 +0000

    Testando link

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -3,7 +3,14 @@ require 'rails_helper'
 feature "Welcomes", type: :feature do
 
   scenario 'Mostra a mensagem de Bem-Vindo' do
-    visit('/')
+    visit(root_path)
     expect(page).to have_content('Bem-Vindo')  
   end
+
+  scenario 'Verificar o link cadastro de clientes' do
+    visit(root_path)
+    #expect(page).to have_selector('ul li a')
+    expect(find('ul li')).to have_link('Cadastro de Clientes')  
+  end
+
 end
